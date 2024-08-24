b2b6d3db4c351640181d3f71c5968714fa1f2f85
Tomas Milukas <tmilukas@gmail.com>
2024-08-24 15:54:27 +0300

    logic done, time for testing

--- src/repositories/pool_repo.rs
+++ src/repositories/pool_repo.rs
@@ -18,7 +18,7 @@ impl PoolRepo {
                 token_a_address, token_b_address, token_a_decimals, token_b_decimals, 
                 tick_spacing, fee_rate, last_updated_at
             ) 
-            VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11)
+            VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12)
             ON CONFLICT (address) DO UPDATE SET
                 name = EXCLUDED.name,
                 token_a_name = EXCLUDED.token_a_name,
@@ -44,6 +44,27 @@ impl PoolRepo {
         .bind(pool.fee_rate)
         .bind(pool.created_at)
         .bind(pool.last_updated_at)
+        .execute(&self.db)
+        .await?;
+
+        Ok(())
+    }
+
+    pub async fn update_liquidity(
+        &self,
+        address: &str,
+        total_liquidity: i64,
+    ) -> Result<(), sqlx::Error> {
+        query(
+            r#"
+            UPDATE pools
+            SET total_liquidity = $1, last_updated_at = $2
+            WHERE address = $3
+            "#,
+        )
+        .bind(total_liquidity)
+        .bind(Utc::now())
+        .bind(address)
         .execute(&self.db)
         .await?;
 
