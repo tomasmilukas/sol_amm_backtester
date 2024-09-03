8b1bc6d0d4c9cba0143211a2177e9ffe5a99df47
Tomas Milukas <tmilukas@gmail.com>
2024-09-03 17:20:25 +0300

    logic is done! time for testing, not looking forward :(

--- src/utils/mod.rs
+++ src/utils/mod.rs
@@ -2,3 +2,4 @@ pub mod decode;
 pub mod transaction_utils;
 pub mod hawksight_parsing_tx;
 pub mod price_calcs;
+pub mod error;
--- src/utils/price_calcs.rs
+++ src/utils/price_calcs.rs
@@ -1,7 +1,13 @@
-pub fn get_current_tick(sqrt_price: f64, tick_spacing: i32) -> i32 {
-    ((sqrt_price.powi(2).log(1.0001)) as i32 / tick_spacing) * tick_spacing
-}
+const Q64: u128 = 1u128 << 64;
 
 pub fn tick_to_sqrt_price(tick: i32) -> f64 {
     1.0001f64.powi(tick / 2)
+}
+
+pub fn f64_to_fixed_point(f64_nmr: f64) -> u128 {
+    (f64_nmr * Q64 as f64).round() as u128
+}
+
+pub fn fixed_point_to_f64(fixed_point: u128) -> f64 {
+    (fixed_point as f64) / (Q64 as f64)
 }
