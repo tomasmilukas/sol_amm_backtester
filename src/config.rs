d9bd614b7b67c8f58c14585202e0203d09e35fac
Tomas Milukas <tmilukas@gmail.com>
2024-09-10 14:01:23 +0300

    random fixes

--- src/config.rs
+++ src/config.rs
@@ -11,6 +11,7 @@ pub enum SyncMode {
 pub struct AppConfig {
     pub database_url: String,
     pub pool_address: String,
+    pub strategy: String,
     pub token_a_amount: u128,
     pub token_b_amount: u128,
     pub range: i32,
@@ -34,6 +35,7 @@ impl AppConfig {
         Ok(Self {
             database_url: env::var("DATABASE_URL").context("DATABASE_URL must be set")?,
             pool_address: env::var("POOL_ADDRESS").context("POOL_ADDRESS must be set")?,
+            strategy: env::var("STRATEGY").context("STRATEGY must be set")?,
             token_a_amount: env::var("TOKEN_A_AMOUNT")
                 .context("TOKEN_A_AMOUNT must be set")?
                 .parse::<u128>()
